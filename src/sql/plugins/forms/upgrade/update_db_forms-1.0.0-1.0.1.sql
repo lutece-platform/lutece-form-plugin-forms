aeda7aa1299295cdcb1145eee9e9d674fade666d
LEAC <fabrice.leac@atos.net>
2018-09-04 12:09:14 +0200

    FORMS-36 : Manage Groups iteration in backoffice

--- src/sql/plugins/forms/upgrade/update_db_forms-1.0.0-1.0.1.sql
+++ src/sql/plugins/forms/upgrade/update_db_forms-1.0.0-1.0.1.sql
@@ -2,3 +2,8 @@ INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_use
 ('116', 'Conditions d''utilisation', 0, 0, 0, 'forms.entryTypeTermsOfService', 'legal', 'forms');
 
 ALTER TABLE forms_form ADD COLUMN display_summary SMALLINT default 0 NOT NULL AFTER breadcrumb_name;
+
+ALTER TABLE forms_group DROP COLUMN iteration_number;
+ALTER TABLE forms_group	ADD COLUMN iteration_min INT  DEFAULT '1' AFTER collapsible;
+ALTER TABLE forms_group	ADD COLUMN iteration_max INT  DEFAULT '1' AFTER iteration_min;
+ALTER TABLE forms_group	ADD COLUMN iteration_label varchar(255) default '' AFTER iteration_max;
