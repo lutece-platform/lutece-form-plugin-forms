dbf3985d60269d530a2653f1fca79e1e85ab3dc0
MOIRE <cyril.moire@atos.net>
2018-10-17 12:29:08 +0200

    formatting

--- src/java/fr/paris/lutece/plugins/forms/web/FormXPage.java
+++ src/java/fr/paris/lutece/plugins/forms/web/FormXPage.java
@@ -922,7 +922,7 @@ public class FormXPage extends MVCApplication
      */
     private void checkIfUserResponseForm( Form form, HttpServletRequest request ) throws SiteMessageException, UserNotSignedException
     {
-        if ( form.isAuthentificationNeeded( ) && form.isOneResponseByUser() )
+        if ( form.isAuthentificationNeeded( ) && form.isOneResponseByUser( ) )
         {
             LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
             int nLimitNumberResponse = FormHome.getNumberOfResponseFormByUser( form.getId( ), user.getName( ) );
