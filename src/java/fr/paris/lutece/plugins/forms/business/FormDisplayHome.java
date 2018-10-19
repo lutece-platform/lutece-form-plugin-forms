lutece-platform/lutece-form-plugin-forms

commit f0a99d805868bd8bae3d5f49815947c2f4e805b4

FORMS-17 : Back Office management of Conditional Display

src/java/fr/paris/lutece/plugins/forms/business/FormDisplayHome.java
@@ -33,12 +33,12 @@
  */
 package fr.paris.lutece.plugins.forms.business;
 
+import java.util.List;
+
 import fr.paris.lutece.portal.service.plugin.Plugin;
 import fr.paris.lutece.portal.service.plugin.PluginService;
 import fr.paris.lutece.portal.service.spring.SpringContextService;
 import fr.paris.lutece.util.ReferenceList;
-
-import java.util.List;
 
 /**
  * This class provides instances management methods (create, find, ...) for FormDisplay objects
@@ -104,7 +104,9 @@ public final class FormDisplayHome
      */
     public static FormDisplay findByPrimaryKey( int nKey )
     {
-        return _dao.load( nKey, _plugin );
+		FormDisplay formDisplay = _dao.load( nKey, _plugin );
+		InitConditionalDisplayForFormDisplay( formDisplay );
+		return formDisplay;
     }
 
     /**
@@ -114,7 +116,12 @@ public final class FormDisplayHome
      */
     public static List<FormDisplay> getFormDisplayList( )
     {
-        return _dao.selectFormDisplayList( _plugin );
+		List<FormDisplay> result = _dao.selectFormDisplayList( _plugin );
+		for ( FormDisplay formDisplay : result )
+		{
+			InitConditionalDisplayForFormDisplay( formDisplay );
+		}
+		return result;
     }
 
     /**
@@ -128,7 +135,12 @@ public final class FormDisplayHome
      */
     public static List<FormDisplay> getFormDisplayListByParent( int nIdStep, int nIdParent )
     {
-        return _dao.selectFormDisplayListByParent( nIdStep, nIdParent, _plugin );
+    	List<FormDisplay> result = _dao.selectFormDisplayListByParent( nIdStep, nIdParent, _plugin );
+		for ( FormDisplay formDisplay : result )
+		{
+			InitConditionalDisplayForFormDisplay( formDisplay );
+		}
+        return result;
     }
 
     /**
@@ -156,6 +168,22 @@ public final class FormDisplayHome
      */
     public static FormDisplay getFormDisplayByFormStepAndComposite( int nIdForm, int nIdStep, int nIdComposite )
     {
-        return _dao.selectFormdisplayByFormStepAndComposite( nIdForm, nIdStep, nIdComposite, _plugin );
+		FormDisplay formDisplay = _dao.selectFormdisplayByFormStepAndComposite( nIdForm, nIdStep, nIdComposite,
+				_plugin );
+		InitConditionalDisplayForFormDisplay( formDisplay );
+		return formDisplay;
     }
+
+	/**
+	 * Inits the Conditional display for a given form display
+	 * 
+	 * @param formDisplay
+	 *            the form display to init
+	 */
+	public static void InitConditionalDisplayForFormDisplay( FormDisplay formDisplay )
+	{
+		formDisplay.setDisplayControl(
+				ControlHome.getConditionalDisplayControlByDisplay( formDisplay.getId() ) );
+	}
+
 }

src/java/fr/paris/lutece/plugins/forms/business/Control.java
@@ -17,6 +17,8 @@ public class Control
 
     private String _strControlType;
 
+	private int _nIdTargetFormDisplay;
+
     /**
      * @return the _nId
      */
@@ -118,4 +120,23 @@ public class Control
     {
         this._strControlType = strControlType;
     }
+
+	/**
+	 * 
+	 * @return the _nIdTargetFormDisplay
+	 */
+	public int getIdTargetFormDisplay()
+	{
+		return _nIdTargetFormDisplay;
+	}
+
+	/**
+	 * 
+	 * @param nIdTargetFormDisplay
+	 *            the nIdTargetFormDisplay to set
+	 */
+	public void setIdTargetFormDisplay( int nIdTargetFormDisplay )
+	{
+		this._nIdTargetFormDisplay = nIdTargetFormDisplay;
+	}
 }

src/java/fr/paris/lutece/plugins/forms/business/ControlDAO.java
@@ -34,13 +34,13 @@
 
 package fr.paris.lutece.plugins.forms.business;
 
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.paris.lutece.portal.service.plugin.Plugin;
 import fr.paris.lutece.util.ReferenceList;
 import fr.paris.lutece.util.sql.DAOUtil;
-import java.sql.Statement;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This class provides Data Access methods for Control objects
@@ -48,11 +48,12 @@ import java.util.List;
 public final class ControlDAO implements IControlDAO
 {
     // Constants
-    private static final String SQL_QUERY_SELECT = "SELECT id_control, value, error_message, id_question, validator_name, control_type FROM forms_control WHERE id_control = ?";
-    private static final String SQL_QUERY_INSERT = "INSERT INTO forms_control ( value, error_message, id_question, validator_name, control_type ) VALUES ( ?, ?, ?, ?, ? ) ";
+	private static final String SQL_QUERY_SELECT = "SELECT id_control, value, error_message, id_question, validator_name, control_type, id_display FROM forms_control WHERE id_control = ?";
+	private static final String SQL_QUERY_SELECT_BY_DISPLAY_ID = "SELECT id_control, value, error_message, id_question, validator_name, control_type, id_display FROM forms_control WHERE id_display = ?";
+	private static final String SQL_QUERY_INSERT = "INSERT INTO forms_control ( value, error_message, id_question, validator_name, control_type, id_display ) VALUES ( ?, ?, ?, ?, ?, ? ) ";
     private static final String SQL_QUERY_DELETE = "DELETE FROM forms_control WHERE id_control = ? ";
-    private static final String SQL_QUERY_UPDATE = "UPDATE forms_control SET id_control = ?, value = ?, error_message = ?, id_question = ?, validator_name = ?, control_type = ? WHERE id_control = ?";
-    private static final String SQL_QUERY_SELECTALL = "SELECT id_control, value, error_message, id_question, validator_name, control_type FROM forms_control";
+	private static final String SQL_QUERY_UPDATE = "UPDATE forms_control SET id_control = ?, value = ?, error_message = ?, id_question = ?, validator_name = ?, control_type = ?, id_display = ? WHERE id_control = ?";
+	private static final String SQL_QUERY_SELECTALL = "SELECT id_control, value, error_message, id_question, validator_name, control_type, id_display FROM forms_control";
     private static final String SQL_QUERY_SELECTALL_ID = "SELECT id_control FROM forms_control";
 
     /**
@@ -70,6 +71,7 @@ public final class ControlDAO implements IControlDAO
             daoUtil.setInt( nIndex++, control.getIdQuestion( ) );
             daoUtil.setString( nIndex++, control.getValidatorName( ) );
             daoUtil.setString( nIndex++, control.getControlType( ) );
+			daoUtil.setInt( nIndex++, control.getIdTargetFormDisplay() );
 
             daoUtil.executeUpdate( );
             if ( daoUtil.nextGeneratedKey( ) )
@@ -105,6 +107,7 @@ public final class ControlDAO implements IControlDAO
             control.setIdQuestion( daoUtil.getInt( nIndex++ ) );
             control.setValidatorName( daoUtil.getString( nIndex++ ) );
             control.setControlType( daoUtil.getString( nIndex++ ) );
+			control.setIdTargetFormDisplay( daoUtil.getInt( nIndex++ ) );
         }
 
         daoUtil.close( );
@@ -138,7 +141,7 @@ public final class ControlDAO implements IControlDAO
         daoUtil.setInt( nIndex++, control.getIdQuestion( ) );
         daoUtil.setString( nIndex++, control.getValidatorName( ) );
         daoUtil.setString( nIndex++, control.getControlType( ) );
-        control.setControlType( daoUtil.getString( nIndex++ ) );
+		daoUtil.setInt( nIndex++, control.getIdTargetFormDisplay() );
 
         daoUtil.setInt( nIndex, control.getId( ) );
 
@@ -167,6 +170,7 @@ public final class ControlDAO implements IControlDAO
             control.setIdQuestion( daoUtil.getInt( nIndex++ ) );
             control.setValidatorName( daoUtil.getString( nIndex++ ) );
             control.setControlType( daoUtil.getString( nIndex++ ) );
+			control.setIdTargetFormDisplay( daoUtil.getInt( nIndex++ ) );
 
             controlList.add( control );
         }
@@ -212,5 +216,32 @@ public final class ControlDAO implements IControlDAO
         daoUtil.close( );
         return controlList;
     }
+
+	@Override
+	public Control selectControlByDisplay( int nIdDisplay, Plugin plugin )
+	{
+
+		DAOUtil daoUtil = new DAOUtil( SQL_QUERY_SELECT_BY_DISPLAY_ID, plugin );
+		daoUtil.setInt( 1, nIdDisplay );
+		daoUtil.executeQuery();
+		Control control = null;
+
+		if ( daoUtil.next() )
+		{
+			control = new Control();
+			int nIndex = 1;
+
+			control.setId( daoUtil.getInt( nIndex++ ) );
+			control.setValue( daoUtil.getString( nIndex++ ) );
+			control.setErrorMessage( daoUtil.getString( nIndex++ ) );
+			control.setIdQuestion( daoUtil.getInt( nIndex++ ) );
+			control.setValidatorName( daoUtil.getString( nIndex++ ) );
+			control.setControlType( daoUtil.getString( nIndex++ ) );
+			control.setIdTargetFormDisplay( daoUtil.getInt( nIndex++ ) );
+		}
+
+		daoUtil.close();
+		return control;
+	}
 
 }

src/java/fr/paris/lutece/plugins/forms/business/ControlHome.java
@@ -33,12 +33,12 @@
  */
 package fr.paris.lutece.plugins.forms.business;
 
+import java.util.List;
+
 import fr.paris.lutece.portal.service.plugin.Plugin;
 import fr.paris.lutece.portal.service.plugin.PluginService;
 import fr.paris.lutece.portal.service.spring.SpringContextService;
 import fr.paris.lutece.util.ReferenceList;
-
-import java.util.List;
 
 /**
  * This class provides instances management methods (create, find, ...) for Control objects
@@ -136,4 +136,17 @@ public final class ControlHome
     {
         return _dao.selectControlsReferenceList( _plugin );
     }
+
+	/**
+	 * Loads an existing control based on the display it is linked to (for
+	 * conditional display)
+	 * 
+	 * @param nIdDisplay
+	 * @return the requested Control
+	 */
+	public static Control getConditionalDisplayControlByDisplay( int nIdDisplay )
+	{
+		return _dao.selectControlByDisplay( nIdDisplay, _plugin );
+	}
+
 }

src/java/fr/paris/lutece/plugins/forms/business/FormDisplay.java
@@ -59,6 +59,8 @@ public class FormDisplay implements Serializable
 
     private int _nDepth;
 
+	private Control _displayControl;
+
     /**
      * @return the _nId
      */
@@ -196,4 +198,24 @@ public class FormDisplay implements Serializable
     {
         _nDepth = nDepth;
     }
+
+	/**
+	 * 
+	 * @return the conditional display control
+	 */
+	public Control getDisplayControl()
+	{
+		return _displayControl;
+	}
+
+	/**
+	 * 
+	 * @param displayControl
+	 *            the control to set
+	 */
+	public void setDisplayControl( Control displayControl )
+	{
+		this._displayControl = displayControl;
+	}
+
 }

src/java/fr/paris/lutece/plugins/forms/business/IControlDAO.java
@@ -34,9 +34,10 @@
 
 package fr.paris.lutece.plugins.forms.business;
 
+import java.util.List;
+
 import fr.paris.lutece.portal.service.plugin.Plugin;
 import fr.paris.lutece.util.ReferenceList;
-import java.util.List;
 
 /**
  * IControlDAO Interface
@@ -113,5 +114,16 @@ public interface IControlDAO
      * @return The referenceList which contains the data of all the control objects
      */
     ReferenceList selectControlsReferenceList( Plugin plugin );
+
+	/**
+	 * Select a control for conditional display based on its attached display id
+	 * 
+	 * @param nIdDisplay
+	 *            the display id
+	 * @param plugin
+	 *            the Plugin
+	 * @return
+	 */
+	Control selectControlByDisplay( int nIdDisplay, Plugin plugin );
 
 }

src/java/fr/paris/lutece/plugins/forms/resources/forms_messages.properties
@@ -222,6 +222,7 @@ manage_questions.columnTitle=Title
 manage_questions.columnDescription=Description
 manage_questions.columnIdEntry=Id entry
 manage_questions.columnType=Type
+manage_questions.columnCondition=Conditional display
 manage_questions.columnAction=Action
 manage_questions.action.modifyQuestion=Modify the question
 manage_questions.action.removeQuestion=Remove the question
@@ -467,6 +468,8 @@ error.moveComposite.notCompleted=An error occured during component move
 error.deleteTransition=An error occured during transition removal
 error.downloadFile=An error occurred during the download of the file
 error.emptyTransition=No selected transition for the control
+error.modifyCondition.question=Please validate the question before saving the conditional display.
+error.modifyCondition.validator=Please select a condition type.
 
 warning.deleteComposite.confirmRemoveActiveForm=The form {0} is active.
 warning.deleteComposite.confirmRemoveQuestion=Do you really want to delete that Question and the associated responses?

src/java/fr/paris/lutece/plugins/forms/resources/forms_messages_fr.properties
@@ -222,6 +222,7 @@ manage_questions.columnTitle=Titre
 manage_questions.columnDescription=Description
 manage_questions.columnIdEntry=Id entry
 manage_questions.columnType=Type
+manage_questions.columnCondition=Affichage conditionnel
 manage_questions.columnAction=Action
 manage_questions.action.modifyQuestion=Modifier la question
 manage_questions.action.removeQuestion=Supprimer la question
@@ -467,6 +468,8 @@ error.moveComposite.notCompleted=Une erreur s'est produite durant le d\u00e9plac
 error.deleteTransition=Une erreur s'est produite durant la suppression de la liaison
 error.downloadFile=Une erreur s'est produite lors du t\u00e9l\u00e9chargement du fichier
 error.emptyTransition=Aucune transition s\u00e9lectionn\u00e9e pour le contr\u00f4le
+error.modifyCondition.question=Veuillez valider la question cible avant d'enregistrer la condition
+error.modifyCondition.validator=Veuillez s\u00e9lectionner un type de condition
 
 warning.deleteComposite.confirmRemoveActiveForm=Le formulaire {0} est actuellement activ\u00e9.
 warning.deleteComposite.confirmRemoveQuestion=Voulez-vous vraiment supprimer cette question et toutes ses r\u00e9ponses associ\u00e9es?

commit c06580d89c6439f1cacc544b51c3b83675f4fb1f

FORMS-17 : fix NPE in FormDisplayHome.findByPrimaryKey when the object doesn't exist (regression from f0a99d805868bd8)

src/java/fr/paris/lutece/plugins/forms/business/FormDisplayHome.java
@@ -105,7 +105,10 @@ public final class FormDisplayHome
     public static FormDisplay findByPrimaryKey( int nKey )
     {
         FormDisplay formDisplay = _dao.load( nKey, _plugin );
-        initConditionalDisplayForFormDisplay( formDisplay );
+        if ( formDisplay != null )
+        {
+            initConditionalDisplayForFormDisplay( formDisplay );
+        }
         return formDisplay;
     }
 