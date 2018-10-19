lutece-platform/lutece-form-plugin-forms

commit e01d9c8d5604cd3f6a7faf0661eed8f62dff3462

FORMS-106 : Add breadcrumb type selection

src/java/fr/paris/lutece/plugins/forms/business/Form.java
@@ -91,6 +91,10 @@ public class Form implements AdminWorkgroupResource, RBACResource
     private int _nIdWorkflow;
 
     private boolean _bAuthentificationNeeded;
+    
+    @NotEmpty( message = "#i18n{forms.validation.form.Breadcrumb.notEmpty}" )
+    @Size( max = 255, message = "#i18n{forms.validation.form.Breadcrumb.size}" )
+    private String _strBreadcrumbName;
 
     /**
      * Returns the Id
@@ -357,6 +361,27 @@ public class Form implements AdminWorkgroupResource, RBACResource
     public void setAuthentificationNeeded( boolean bAuthentificationNeeded )
     {
         this._bAuthentificationNeeded = bAuthentificationNeeded;
+    }
+    
+    /**
+     * Returns the BreadcrumbName
+     * 
+     * @return The BreadcrumbName
+     */
+    public String getBreadcrumbName( )
+    {
+        return _strBreadcrumbName;
+    }
+
+    /**
+     * Sets the Title
+     * 
+     * @param strBreadcrumbName
+     *            The breadcrumb bean name
+     */
+    public void setBreadcrumbName( String strBreadcrumbName )
+    {
+        _strBreadcrumbName = strBreadcrumbName;
     }
 
 }

src/java/fr/paris/lutece/plugins/forms/business/FormDAO.java
@@ -50,11 +50,11 @@ import fr.paris.lutece.util.sql.DAOUtil;
 public final class FormDAO implements IFormDAO
 {
     // Constants
-    private static final String SQL_QUERY_SELECT = "SELECT id_form, title, description, creation_date, update_date, availability_start_date, availability_end_date, workgroup, id_workflow, authentification_needed FROM forms_form WHERE id_form = ?";
-    private static final String SQL_QUERY_INSERT = "INSERT INTO forms_form ( title, description, update_date, availability_start_date, availability_end_date, workgroup, id_workflow, authentification_needed ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? ) ";
+    private static final String SQL_QUERY_SELECT = "SELECT id_form, title, description, creation_date, update_date, availability_start_date, availability_end_date, workgroup, id_workflow, authentification_needed, breadcrumb_name FROM forms_form WHERE id_form = ?";
+    private static final String SQL_QUERY_INSERT = "INSERT INTO forms_form ( title, description, update_date, availability_start_date, availability_end_date, workgroup, id_workflow, authentification_needed, breadcrumb_name ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? ) ";
     private static final String SQL_QUERY_DELETE = "DELETE FROM forms_form WHERE id_form = ? ";
-    private static final String SQL_QUERY_UPDATE = "UPDATE forms_form SET id_form = ?, title = ?, description = ?, update_date = ?, availability_start_date = ?, availability_end_date = ?, workgroup = ?, id_workflow = ?, authentification_needed = ? WHERE id_form = ?";
-    private static final String SQL_QUERY_SELECTALL = "SELECT id_form, title, description, creation_date,update_date, availability_start_date, availability_end_date, workgroup, id_workflow, authentification_needed FROM forms_form";
+    private static final String SQL_QUERY_UPDATE = "UPDATE forms_form SET id_form = ?, title = ?, description = ?, update_date = ?, availability_start_date = ?, availability_end_date = ?, workgroup = ?, id_workflow = ?, authentification_needed = ?, breadcrumb_name = ? WHERE id_form = ?";
+    private static final String SQL_QUERY_SELECTALL = "SELECT id_form, title, description, creation_date,update_date, availability_start_date, availability_end_date, workgroup, id_workflow, authentification_needed, breadcrumb_name FROM forms_form";
     private static final String SQL_QUERY_SELECTALL_ID = "SELECT id_form FROM forms_form";
 
     /**
@@ -76,6 +76,7 @@ public final class FormDAO implements IFormDAO
             daoUtil.setString( nIndex++, form.getWorkgroup( ) );
             daoUtil.setInt( nIndex++, form.getIdWorkflow( ) );
             daoUtil.setBoolean( nIndex++, form.getAuthentificationNeeded( ) );
+            daoUtil.setString( nIndex++, form.getBreadcrumbName( ) );
             daoUtil.executeUpdate( );
             if ( daoUtil.nextGeneratedKey( ) )
             {
@@ -115,7 +116,7 @@ public final class FormDAO implements IFormDAO
             form.setWorkgroup( daoUtil.getString( nIndex++ ) );
             form.setIdWorkflow( daoUtil.getInt( nIndex++ ) );
             form.setAuthentificationNeeded( daoUtil.getBoolean( nIndex++ ) );
-
+            form.setBreadcrumbName( daoUtil.getString( nIndex++ ) );
         }
 
         daoUtil.close( );
@@ -155,6 +156,7 @@ public final class FormDAO implements IFormDAO
         daoUtil.setString( nIndex++, form.getWorkgroup( ) );
         daoUtil.setInt( nIndex++, form.getIdWorkflow( ) );
         daoUtil.setBoolean( nIndex++, form.getAuthentificationNeeded( ) );
+        daoUtil.setString( nIndex++, form.getBreadcrumbName( ) );
         daoUtil.setInt( nIndex++, form.getId( ) );
 
         daoUtil.executeUpdate( );
@@ -186,6 +188,7 @@ public final class FormDAO implements IFormDAO
             form.setWorkgroup( daoUtil.getString( nIndex++ ) );
             form.setIdWorkflow( daoUtil.getInt( nIndex++ ) );
             form.setAuthentificationNeeded( daoUtil.getBoolean( nIndex++ ) );
+            form.setBreadcrumbName( daoUtil.getString( nIndex++ ) );
             formList.add( form );
         }
 

src/java/fr/paris/lutece/plugins/forms/business/IStepDAO.java
@@ -117,6 +117,17 @@ public interface IStepDAO
     List<Integer> selectIdStepsList( Plugin plugin );
 
     /**
+     * Load the id of all the step linked a given parent form and returns them as a list
+     * 
+     * @param nFormId
+     *            the parent form identifier
+     * @param plugin
+     *            the Plugin
+     * @return The List which contains the id of all the step objects
+     */
+    List<Integer> selectIdStepsListByForm( int nFormId, Plugin plugin );
+
+    /**
      * Load the data of all the step objects and returns them as a referenceList
      * 
      * @param plugin
@@ -146,4 +157,5 @@ public interface IStepDAO
      * @return The Referencelist which contains the id and titles of all the step objects
      */
     ReferenceList selectStepReferenceListbyForm( int nFormId, Plugin plugin );
+
 }

src/java/fr/paris/lutece/plugins/forms/business/StepDAO.java
@@ -56,6 +56,7 @@ public final class StepDAO implements IStepDAO
     private static final String SQL_QUERY_SELECTALL = "SELECT id_step, title, description, id_form, is_initial, is_final FROM forms_step";
     private static final String SQL_QUERY_SELECTALL_BY_FORM = "SELECT id_step, title, description, id_form, is_initial, is_final FROM forms_step where id_form = ?";
     private static final String SQL_QUERY_SELECTALL_ID = "SELECT id_step FROM forms_step";
+    private static final String SQL_QUERY_SELECTALL_ID_BY_FORM = "SELECT id_step FROM forms_step WHERE id_form = ?";
 
     /**
      * {@inheritDoc }
@@ -212,6 +213,26 @@ public final class StepDAO implements IStepDAO
     {
         List<Integer> stepList = new ArrayList<Integer>( );
         DAOUtil daoUtil = new DAOUtil( SQL_QUERY_SELECTALL_ID, plugin );
+        daoUtil.executeQuery( );
+
+        while ( daoUtil.next( ) )
+        {
+            stepList.add( daoUtil.getInt( 1 ) );
+        }
+
+        daoUtil.close( );
+        return stepList;
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public List<Integer> selectIdStepsListByForm( int nIdForm, Plugin plugin )
+    {
+        List<Integer> stepList = new ArrayList<Integer>( );
+        DAOUtil daoUtil = new DAOUtil( SQL_QUERY_SELECTALL_ID_BY_FORM, plugin );
+        daoUtil.setInt( 1, nIdForm );
         daoUtil.executeQuery( );
 
         while ( daoUtil.next( ) )

src/java/fr/paris/lutece/plugins/forms/business/StepHome.java
@@ -166,6 +166,19 @@ public final class StepHome
     }
 
     /**
+     * Load the id of all the step objects linked to a given form and returns them as a list
+     * 
+     * @param nIdForm
+     *            the form identifier
+     * 
+     * @return the list which contains the id of all the step objects
+     */
+    public static List<Integer> getIdStepsListByForm( int nIdForm )
+    {
+        return _dao.selectIdStepsListByForm( nIdForm, _plugin );
+    }
+
+    /**
      * Load the data of all the step objects and returns them as a referenceList
      * 
      * @return the referenceList which contains the data of all the step objects

src/java/fr/paris/lutece/plugins/forms/resources/forms_messages.properties
@@ -66,6 +66,8 @@ create_form.labelAuthentificationNeeded=Authentication required
 create_form.labelAuthentificationNeeded.help=
 create_form.workflow.label=Workflow
 create_form.workflow.label.help=Select the workflow linked to the form
+create_form.labelBreadcrumb=Breadcrumb type
+create_form.labelBreadcrumb.help=
 modify_form.pagetitle=Modify Form
 modify_form.title=Modify Form :
 modify_form.labelTitle=Title
@@ -84,6 +86,8 @@ modify_form.tab.labelPublication=Publishing
 modify_form.tab.labelSteps=Steps
 modify_form.workflow.label=Workflow
 modify_form.workflow.label.help=Select the workflow linked to the form
+modify_form.labelBreadcrumb=Breadcrumb type
+modify_form.labelBreadcrumb.help=
 message.confirmRemoveForm=Are you sure that you want to delete this form?
 message.confirmRemoveActiveForm=This form is currently enabled. Are you sure that you want to delete it ?
 
@@ -96,6 +100,8 @@ validation.form.Description.size=The field Description cannot accept more than 2
 validation.form.CreationDate.size=The field Creation date cannot accept more than 20 characters.
 validation.form.StartDate.size=The field Start date cannot accept more than 20 characters.
 validation.form.EndDate.size=The field End date cannot accept more than 20 characters.
+validation.form.Breadcrumb.notEmpty=The field Breadcrumb type cannot be empty. Please fill it.
+validation.form.Breadcrumb.size=The field Breadcrumb type cannot accept more than 255 characters.
 
 # model attributes for validation messages
 model.entity.form.attribute.title=Title
@@ -464,6 +470,10 @@ moveComposite.position.notAvailable=Not available. Please select a parent group.
 # Validator display names
 validator.pattern.name=Regular expression
 validator.listValue.name=Exact value
+
+#Breadcrumb display name
+breadcrumb.horizontal.name=Horizontal breadcrumb
+breadcrumb.vertical.name=Vertical breadcrumb
 
 # Conditional display management keys
  manageCondition.question.label=Target question

src/java/fr/paris/lutece/plugins/forms/resources/forms_messages_fr.properties
@@ -66,6 +66,8 @@ create_form.labelAuthentificationNeeded=Authentification requise
 create_form.labelAuthentificationNeeded.help=
 create_form.workflow.label=Workflow
 create_form.workflow.label.help=S\u00e9lectionner le workflow associ\u00e9 au formulaire
+create_form.labelBreadcrumb=Type de fil d'ariane
+create_form.labelBreadcrumb.help=
 modify_form.pageTitle=Modification du formulaire
 modify_form.title=Modification du formulaire :
 modify_form.labelTitle=Titre
@@ -84,6 +86,8 @@ modify_form.tab.labelPublication=Publication
 modify_form.tab.labelSteps=Etapes
 modify_form.workflow.label=Workflow
 modify_form.workflow.label.help=S\u00e9lectionner le workflow associ\u00e9 au formulaire
+modify_form.labelBreadcrumb=Type de fil d'ariane
+modify_form.labelBreadcrumb.help=
 message.confirmRemoveForm=Etes vous s\u00fbr(e) de vouloir supprimer ce formulaire ?
 message.confirmRemoveActiveForm=Ce formulaire est actuellement activ\u00e9. Etes vous s\u00fbr(e) de vouloir le supprimer ?
 
@@ -96,6 +100,8 @@ validation.form.Description.size=Le champ Description ne doit pas contenir plus 
 validation.form.CreationDate.size=Le champ Creation date ne doit pas contenir plus de 20 caract\u00e8res.
 validation.form.StartDate.size=Le champ Start date ne doit pas contenir plus de 20 caract\u00e8res.
 validation.form.EndDate.size=Le champ End date ne doit pas contenir plus de 20 caract\u00e8res.
+validation.form.Breadcrumb.notEmpty=Le champ Type de fil d'ariane ne doit pas \u00eatre vide. Veuillez le remplir SVP.
+validation.form.Breadcrumb.size=Le champ Type de fil d'ariane ne doit pas contenir plus de 255 caract\u00e8res.
 
 # model attributes for validation messages
 model.entity.form.attribute.title=Title
@@ -464,6 +470,10 @@ moveComposite.position.notAvailable=Non disponible. Veuillez s\u00e9lectionner u
 # Validator display names
 validator.pattern.name=Expression r\u00e9guli\u00e8re
 validator.listValue.name=Valeur exacte
+
+#Breadcrumb display name
+breadcrumb.horizontal.name=Fil d'ariane horizontal
+breadcrumb.vertical.name=Fil d'ariane vertical
 
 # Conditional display management keys
  manageCondition.question.label=Question cible

src/java/fr/paris/lutece/plugins/forms/util/FormsConstants.java
@@ -70,7 +70,8 @@ public final class FormsConstants
     public static final String MARK_DISPLAY_ORDER = "displayOrder";
     public static final String MARK_TRANSITION = "transition";
     public static final String MARK_CONTROL = "control";
-    public static final String MARK_FORM_BREADCRUMB = "formBreadcrumb";
+    public static final String MARK_FORM_TOP_BREADCRUMB = "formTopBreadcrumb";
+    public static final String MARK_FORM_BOTTOM_BREADCRUMB = "formBottomBreadcrumb";
     public static final String MARK_QUESTION_CONTENT = "questionContent";
     public static final String MARK_INFO = "messageInfo";
     public static final String MARK_CONTROL_TEMPLATE = "control_template";

commit cf60aabb1fc0840c058dd2c6a5f25b9ef43f3494

FORMS-106: fix FormBusinessTest (regression from e01d9c8d5604cd3f)

src/test/java/fr/paris/lutece/plugins/forms/business/FormBusinessTest.java
@@ -51,6 +51,8 @@ public class FormBusinessTest extends LuteceTestCase
     private static final Date STARTDATE2 = java.sql.Date.valueOf( "1970-01-02" );
     private static final Date ENDDATE1 = java.sql.Date.valueOf( "1970-01-05" );
     private static final Date ENDDATE2 = java.sql.Date.valueOf( "1970-01-06" );
+    private static final String BREADCRUMB1 = "forms.horizontalBreadcrumb";
+    private static final String BREADCRUMB2 = "forms.verticalBreadcrumb";
 
     /**
      * test Form
@@ -63,6 +65,7 @@ public class FormBusinessTest extends LuteceTestCase
         form.setDescription( DESCRIPTION1 );
         form.setAvailabilityStartDate( STARTDATE1 );
         form.setAvailabilityEndDate( ENDDATE1 );
+        form.setBreadcrumbName( BREADCRUMB1 );
 
         // Create test
         FormHome.create( form );
@@ -71,18 +74,21 @@ public class FormBusinessTest extends LuteceTestCase
         assertEquals( formStored.getDescription( ), form.getDescription( ) );
         assertEquals( formStored.getAvailabilityStartDate( ).getTime( ), form.getAvailabilityStartDate( ).getTime( ) );
         assertEquals( formStored.getAvailabilityEndDate( ), form.getAvailabilityEndDate( ) );
+        assertEquals( formStored.getBreadcrumbName( ), form.getBreadcrumbName( ) );
 
         // Update test
         form.setTitle( TITLE2 );
         form.setDescription( DESCRIPTION2 );
         form.setAvailabilityStartDate( STARTDATE2 );
         form.setAvailabilityEndDate( ENDDATE2 );
+        form.setBreadcrumbName( BREADCRUMB2 );
         FormHome.update( form );
         formStored = FormHome.findByPrimaryKey( form.getId( ) );
         assertEquals( formStored.getTitle( ), form.getTitle( ) );
         assertEquals( formStored.getDescription( ), form.getDescription( ) );
         assertEquals( formStored.getAvailabilityStartDate( ), form.getAvailabilityStartDate( ) );
         assertEquals( formStored.getAvailabilityEndDate( ), form.getAvailabilityEndDate( ) );
+        assertEquals( formStored.getBreadcrumbName( ), form.getBreadcrumbName( ) );
 
         // List test
         FormHome.getFormList( );