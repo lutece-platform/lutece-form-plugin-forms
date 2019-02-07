44d401b67810f9d8014a22172ade323cb1e15baa
Laurent HOHL <laurent.hohl@gmail.com>
2019-02-07 13:58:24 +0100

    FORMS-248 : Add options on manage form and imporve skin

--- src/sql/plugins/forms/upgrade/update_db_forms-1.0.1-1.0.2.sql
+++ src/sql/plugins/forms/upgrade/update_db_forms-1.0.1-1.0.2.sql
@@ -10,3 +10,10 @@ CREATE TABLE forms_indexer_action (
   id_task INT DEFAULT 0 NOT NULL ,
   PRIMARY KEY (id_action)
 );
+
+REPLACE INTO `forms_action` VALUES (1, 'forms.action.modify.name', 'forms.action.modify.description', 'jsp/admin/plugins/forms/ManageSteps.jsp?view=manageSteps', 'pencil', 'MODIFY', 0);
+REPLACE INTO `forms_action` VALUES (2, 'forms.action.params.name', 'forms.action.params.description', 'jsp/admin/plugins/forms/ManageForms.jsp?view=modifyForm', 'cog', 'PARAM', 0);
+REPLACE INTO `forms_action` VALUES (3, 'forms.action.publish.name', 'forms.action.publish.description', 'jsp/admin/plugins/forms/ManageForms.jsp?view=modifyPublication', 'calendar', 'PUBLISH', 0);
+REPLACE INTO `forms_action` VALUES (4, 'forms.action.delete.name', 'forms.action.delete.description', 'jsp/admin/plugins/forms/ManageForms.jsp?view=confirmRemoveForm', 'trash', 'DELETE', 0);
+REPLACE INTO `forms_action` VALUES (100, 'module.forms.documentproducer.actions.extractpdf.name', 'module.forms.documentproducer.actions.extractpdf.description', 'jsp/admin/plugins/forms/modules/documentproducer/ManageConfigProducer.jsp?view=getManageConfigProducer', 'file-pdf-o', 'PDFPROD', 0);
+
