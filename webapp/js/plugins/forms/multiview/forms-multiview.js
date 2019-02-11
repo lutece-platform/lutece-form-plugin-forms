22570a7dec4b1360f6ee6befb4e573f12db6e7ca
Laurent HOHL <laurent.hohl@gmail.com>
2019-02-11 16:51:02 +0100

    FORMS-284 : Improve multiview

--- webapp/js/plugins/forms/multiview/forms-multiview.js
+++ webapp/js/plugins/forms/multiview/forms-multiview.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
+$( function() {
 	// Set link on whole tr
 	$("tbody > tr").on( 'click', function(e){
 		redirectOnClick(this);
 	});
+	
+	var q = $("#searched_text").val().trim();
+	if ( q !='' ){
+		$("#search-text .input-group-btn > button").after('<button type="button" onclick="resetForm();" class="btn btn-danger"><i class="fa fa-remove"></i></button>');
+	}
 });
     	
 function redirectOnClick( element ){
@@ -13,4 +18,12 @@ function redirectOnClick( element ){
 		form.appendTo('body');
 		form.submit().remove();
 	}
+};
+
+function resetForm( ){
+	$("#searched_text").val('');
+	var filter='#' + $('#formsFilter').val(), filter_to=filter+'_to', filter_from=filter+'_from';
+	$(filter_from).val ('');
+  	$(filter_to).val ('');
+	$("#searchForm").submit();
 };
