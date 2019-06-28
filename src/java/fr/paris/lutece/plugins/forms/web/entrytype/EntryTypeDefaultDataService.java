727ea6681a9abf2199cb3910661e58fc09d4c099
Jean-Baptiste Vanhems <jbvanhems@gmail.com>
2019-06-28 08:27:13 +0200

    FORMS-386 : add doSynchronousUploadDocument

--- src/java/fr/paris/lutece/plugins/forms/web/entrytype/EntryTypeDefaultDataService.java
+++ src/java/fr/paris/lutece/plugins/forms/web/entrytype/EntryTypeDefaultDataService.java
@@ -140,7 +140,7 @@ public class EntryTypeDefaultDataService implements IEntryDataService
         }
         else
         {
-            formQuestionResponse.setError( null );
+            formQuestionResponse.setError( error );
         }
 
         return formQuestionResponse;
