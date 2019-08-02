98d248d9117efa0aec7d4b212976c854521456f5
Rémi Zara <remi.zara@paris.fr>
2019-08-02 12:59:35 +0200

    FORMS-426: Correct typo in popup options
    The scrollbar would not appear with older browsers (IE11, FireFox 45)

--- webapp/js/plugins/forms/terms-of-service.js
+++ webapp/js/plugins/forms/terms-of-service.js
@@ -1,5 +1,5 @@
 function openFormsTermsOfService(url)
 {
-  var forms_win_terms_of_service = self.open(url, 'forms_terms_of_service', 'toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbar=yes,resizable=yes,copyhistory=yes,width=500px,height=300px');
+  var forms_win_terms_of_service = self.open(url, 'forms_terms_of_service', 'toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=yes,resizable=yes,copyhistory=yes,width=500px,height=300px');
   forms_win_terms_of_service.focus();
 }
