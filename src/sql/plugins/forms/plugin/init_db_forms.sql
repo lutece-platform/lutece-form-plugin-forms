3079b37583739873af0b0d3bef282e3fc3c37af7
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-09-08 09:36:52 +0200

    FORMS-564 : Corrects script

--- src/sql/plugins/forms/plugin/init_db_forms.sql
+++ src/sql/plugins/forms/plugin/init_db_forms.sql
@@ -1,7 +1,7 @@
 --
 -- Dumping data for table `forms_action`
 --
-TRUNCATE forms_action;
+DELETE FROM forms_action;
 INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon_url, action_permission, form_state) VALUES (1,'forms.action.modify.name','forms.action.modify.description','jsp/admin/plugins/forms/ManageSteps.jsp?view=manageSteps','edit','MODIFY',0);
 INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon_url, action_permission, form_state) VALUES (3, 'forms.action.params.name', 'forms.action.params.description', 'jsp/admin/plugins/forms/ManageForms.jsp?view=modifyForm', 'cog', 'PARAM', 0);
 INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon_url, action_permission, form_state) VALUES (2,'forms.action.delete.name','forms.action.delete.description','jsp/admin/plugins/forms/ManageForms.jsp?view=confirmRemoveForm','trash','DELETE',0);
@@ -10,7 +10,7 @@ INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon
 --
 -- Dumping data for table `forms_global_action`
 --
-TRUNCATE forms_global_action;
+DELETE FROM forms_global_action;
 INSERT INTO forms_global_action (id_action, code, name_key, description_key, action_url, icon_url ) VALUES (1,'multiviewconfig','forms.action.multiviewConfig.labelKey','forms.action.multiviewConfig.descriptionKey','jsp/admin/plugins/forms/MultiviewForms.jsp?view=view_multiview_config','edit');
 INSERT INTO forms_global_action (id_action, code, name_key, description_key, action_url, icon_url ) VALUES (2,'multiviewExport','forms.action.multiviewExport.labelKey','forms.action.multiviewExport.descriptionKey','jsp/admin/plugins/forms/MultiviewForms.jsp','edit');
 
