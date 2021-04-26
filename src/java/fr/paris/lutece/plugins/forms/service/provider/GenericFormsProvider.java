16052875d2dd34dcf8c00a786e2ee8278891689e
Guillaume Lépine <guillaume.lepine@sogeti.com>
2021-04-26 15:37:37 +0200

    FORMS-518: create Generic Form Provider

---
+++ src/java/fr/paris/lutece/plugins/forms/service/provider/GenericFormsProvider.java
@@ -0,0 +1,156 @@
+package fr.paris.lutece.plugins.forms.service.provider;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import fr.paris.lutece.plugins.forms.business.Form;
+import fr.paris.lutece.plugins.forms.business.FormQuestionResponse;
+import fr.paris.lutece.plugins.forms.business.FormQuestionResponseHome;
+import fr.paris.lutece.plugins.forms.business.FormResponse;
+import fr.paris.lutece.plugins.forms.business.FormResponseHome;
+import fr.paris.lutece.plugins.forms.business.Question;
+import fr.paris.lutece.plugins.forms.business.QuestionHome;
+import fr.paris.lutece.plugins.forms.service.download.FormsFileDownloadProvider;
+import fr.paris.lutece.plugins.forms.service.entrytype.EntryTypeComment;
+import fr.paris.lutece.plugins.forms.util.FormsConstants;
+import fr.paris.lutece.plugins.forms.web.admin.MultiviewFormResponseDetailsJspBean;
+import fr.paris.lutece.plugins.genericattributes.business.Entry;
+import fr.paris.lutece.plugins.genericattributes.business.Field;
+import fr.paris.lutece.plugins.genericattributes.service.entrytype.EntryTypeServiceManager;
+import fr.paris.lutece.plugins.genericattributes.service.entrytype.IEntryTypeService;
+import fr.paris.lutece.plugins.workflowcore.business.resource.ResourceHistory;
+import fr.paris.lutece.plugins.workflowcore.service.provider.IProvider;
+import fr.paris.lutece.plugins.workflowcore.service.provider.InfoMarker;
+import fr.paris.lutece.portal.service.download.AbstractFileDownloadProvider;
+import fr.paris.lutece.portal.service.download.FileDownloadData;
+import fr.paris.lutece.portal.service.download.IFileDownloadProvider;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.util.AppLogService;
+import fr.paris.lutece.portal.service.util.AppPathService;
+import fr.paris.lutece.util.url.UrlItem;
+
+public abstract class GenericFormsProvider implements IProvider
+{
+    // MESSAGE
+    private static final String MESSAGE_DESCRIPTION = "forms.marker.provider.url.detail.reponse.description";
+
+    // MARKS
+    private static final String MARK_POSITION = "position_";
+    private static final String MARK_URL_ADMIN_RESPONSE = "url_admin_forms_response_detail";
+    // PARAMETERS
+    public static final String PARAMETER_VIEW_FORM_RESPONSE_DETAILS = "view_form_response_details";
+    public static final String PARAMETER_ID_FORM_RESPONSES = "id_form_response";
+
+    // FIELDS
+    protected final FormResponse _formResponse;
+    private final HttpServletRequest _request;
+
+    public GenericFormsProvider( ResourceHistory resourceHistory, HttpServletRequest request )
+    {
+        // Get the form response from the resourceHistory
+        _formResponse = FormResponseHome.findByPrimaryKey( resourceHistory.getIdResource( ) );
+        _request = request;
+    }
+
+    @Override
+    public String provideDemandId( )
+    {
+        return String.valueOf( _formResponse.getId( ) );
+    }
+
+    @Override
+    public String provideDemandSubtypeId( )
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<InfoMarker> provideMarkerValues( )
+    {
+        Collection<InfoMarker> result = new ArrayList<>( );
+
+        List<FormQuestionResponse> listFormQuestionResponse = FormQuestionResponseHome.getFormQuestionResponseListByFormResponse( _formResponse.getId( ) );
+
+        Map<Integer, InfoMarker> markers = new HashMap<>( );
+        for ( FormQuestionResponse formQuestionResponse : listFormQuestionResponse )
+        {
+            InfoMarker notifyMarker = markers.computeIfAbsent( formQuestionResponse.getQuestion( ).getId( ), ( k ) -> new InfoMarker( MARK_POSITION
+                    + formQuestionResponse.getQuestion( ).getId( ) ) );
+            IEntryTypeService entryTypeService = EntryTypeServiceManager.getEntryTypeService( formQuestionResponse.getQuestion( ).getEntry( ) );
+            String value = "";
+            if ( entryTypeService instanceof EntryTypeComment )
+            {
+                Entry entry = formQuestionResponse.getQuestion( ).getEntry( );
+                Field fieldFile = entry.getFieldByCode( IEntryTypeService.FIELD_DOWNLOADABLE_FILE );
+                if ( fieldFile != null )
+                {
+                    FileDownloadData fileDownloadData = new FileDownloadData( entry.getIdResource( ), Form.RESOURCE_TYPE, Integer.parseInt( fieldFile.getValue( ) ) );
+                    IFileDownloadProvider provider = AbstractFileDownloadProvider.findProvider( FormsFileDownloadProvider.PROVIDER_NAME );
+                    value = provider.getDownloadUrl( fileDownloadData, false );                     
+                }
+            }
+            else if ( CollectionUtils.isNotEmpty( formQuestionResponse.getEntryResponse( ) ) )
+            {
+                value = formQuestionResponse.getEntryResponse( ).stream( )
+                        .map( response -> entryTypeService.getResponseValueForRecap(
+                                formQuestionResponse.getQuestion( ).getEntry( ), _request, response, null ) )
+                        .collect( Collectors.joining( ", " ) );
+            }
+            if ( notifyMarker.getValue( ) == null )
+            {
+                notifyMarker.setValue( value );
+            }
+            else
+            {
+                notifyMarker.setValue( notifyMarker.getValue( ) + "<br>" + value );
+            }
+            AppLogService.debug( "Adding infomarker " + notifyMarker.getMarker( ) + "=" + notifyMarker.getValue( ) );
+        }
+        result.addAll( markers.values( ) );
+
+        InfoMarker notifyMarkerUrl = new InfoMarker( MARK_URL_ADMIN_RESPONSE );
+        UrlItem url = new UrlItem( AppPathService.getBaseUrl( _request ) + MultiviewFormResponseDetailsJspBean.CONTROLLER_JSP_NAME_WITH_PATH );
+        url.addParameter( FormsConstants.PARAMETER_TARGET_VIEW, PARAMETER_VIEW_FORM_RESPONSE_DETAILS );
+        url.addParameter( PARAMETER_ID_FORM_RESPONSES, _formResponse.getId( ) );
+        notifyMarkerUrl.setValue( url.getUrl( ) );
+        result.add( notifyMarkerUrl );
+        return result;
+    }
+
+    /**
+     * Get the collection of InfoMarker, for the given form
+     * 
+     * @param form
+     *            The form
+     * @return the collection of the notifyMarkers
+     */
+    public static Collection<InfoMarker> getProviderMarkerDescriptions( Form form )
+    {
+        Collection<InfoMarker> collectionNotifyMarkers = new ArrayList<>( );
+
+        List<Question> listFormQuestions = QuestionHome.getListQuestionByIdForm( form.getId( ) );
+
+        for ( Question formQuestion : listFormQuestions )
+        {
+            InfoMarker notifyMarker = new InfoMarker( MARK_POSITION + formQuestion.getId( ) );
+            notifyMarker.setDescription( formQuestion.getTitle( ) );
+            collectionNotifyMarkers.add( notifyMarker );
+        }
+        InfoMarker notifyMarkerURl = new InfoMarker( MARK_URL_ADMIN_RESPONSE );
+        notifyMarkerURl.setDescription( I18nService.getLocalizedString( MESSAGE_DESCRIPTION, I18nService.getDefaultLocale( ) ) );
+        collectionNotifyMarkers.add( notifyMarkerURl );
+        return collectionNotifyMarkers;
+    }
+
+}
