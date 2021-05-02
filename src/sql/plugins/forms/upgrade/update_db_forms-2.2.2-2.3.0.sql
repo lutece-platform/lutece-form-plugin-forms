c45de970e75e3927d2769bc6ad1620fa4fe70962
Rémi Zara <remi.zara@paris.fr>
2021-05-02 19:50:22 +0200

    FORMS-639: Add a telephone number entry type
    Relies on com.googlecode.libphonenumber for telephone number managment.
    The telephone number is parsed and displayed according to the region specified at
    question creation time (for instance FR for french numbers, or IT for italian
    numbers).
    The telephone number is store normalized (E164, which is international format
    with no formatting).

---
+++ src/sql/plugins/forms/upgrade/update_db_forms-2.2.2-2.3.0.sql
@@ -0,0 +1,2 @@
+INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+(119, 'Numéro de téléphone', 0, 0, 0, 'forms.entryTypeTelephoneNumber', 'phone-square', 'forms',19,0);
--- src/sql/plugins/genericattributes/plugin/init_db_generic_attributes_forms.sql
+++ src/sql/plugins/genericattributes/plugin/init_db_generic_attributes_forms.sql
@@ -37,3 +37,5 @@ INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_use
 (117, 'Lecture automatique fichier (OCR)', 0, 0, 0, 'forms.entryTypeAutomaticFileReading', 'file', 'forms',18,1);
 INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
 (118, 'Camera', 0, 0, 0, 'forms.entryTypeCamera', 'camera', 'forms',15,0);
+INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+(119, 'Numéro de téléphone', 0, 0, 0, 'forms.entryTypeTelephoneNumber', 'phone-square', 'forms',19,0);
