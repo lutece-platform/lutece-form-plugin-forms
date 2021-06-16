d2a10f370aeacac5441a608215f19c6ba8efa0d0
HOHL <laurent.hohl@gmail.com>
2021-06-16 14:55:44 +0200

    FORMS-659 : Update scripts action

--- src/sql/plugins/forms/upgrade/update_db_forms-2.2.0-2.2.1.sql
+++ src/sql/plugins/forms/upgrade/update_db_forms-2.2.0-2.2.1.sql
@@ -4,3 +4,7 @@ INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon
 INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon_url, action_permission, form_state) VALUES (7, 'forms.action.json.download.name', 'forms.json.download.description', 'jsp/admin/plugins/forms/ManageForms.jsp?action=doExportJson', 'download', 'MODIFY', 0);
 
 DELETE FROM forms_display WHERE id_parent = -1;
+
+DELETE FROM forms_action WHERE id_action IN (1,3);
+INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon_url, action_permission, form_state) VALUES (1,'forms.action.modify.name','forms.action.modify.description','jsp/admin/plugins/forms/ManageForms.jsp?view=modifyForm','edit','MODIFY',0);
+INSERT INTO forms_action (id_action, name_key, description_key, action_url, icon_url, action_permission, form_state) VALUES (3, 'forms.action.params.name', 'forms.action.params.description', 'jsp/admin/plugins/forms/ManageSteps.jsp?view=manageSteps', 'cog', 'PARAM', 0);
