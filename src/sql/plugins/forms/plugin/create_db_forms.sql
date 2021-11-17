9fea4fae7ea233ac00974aab45a174d7da96edca
Guillaume Lépine <guillaume.lepine@sogeti.com>
2021-11-17 11:40:00 +0100

    FORMS-712: Fix script

--- src/sql/plugins/forms/plugin/create_db_forms.sql
+++ src/sql/plugins/forms/plugin/create_db_forms.sql
@@ -26,7 +26,7 @@ captcha_recap SMALLINT default 0,
 count_responses SMALLINT default 0,
 label_final_button VARCHAR(255),
 unavailable_message VARCHAR(255),
-id_logo INT NOT NULL DEFAULT 0,
+id_logo INT DEFAULT 0 NOT NULL,
 PRIMARY KEY (id_form)
 );
 
