629034983f18b01cb83e4e4791d08159e9f9cda5
Guillaume Lépine <guillaume.lepine@sogeti.com>
2022-02-07 10:54:56 +0100

    FORMS-745: Add workflow actions to FO FormResponseXPage

--- src/java/fr/paris/lutece/plugins/forms/web/FormResponseXPage.java
+++ src/java/fr/paris/lutece/plugins/forms/web/FormResponseXPage.java
@@ -1,5 +1,8 @@
 package fr.paris.lutece.plugins.forms.web;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.Locale;
 import java.util.Map;
 
@@ -7,15 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import fr.paris.lutece.api.user.User;
+import fr.paris.lutece.plugins.forms.business.Form;
+import fr.paris.lutece.plugins.forms.business.FormHome;
 import fr.paris.lutece.plugins.forms.business.FormResponse;
 import fr.paris.lutece.plugins.forms.business.FormResponseHome;
+import fr.paris.lutece.plugins.forms.service.upload.FormsAsynchronousUploadHandler;
 import fr.paris.lutece.plugins.forms.util.FormsConstants;
+import fr.paris.lutece.plugins.workflowcore.business.action.Action;
 import fr.paris.lutece.portal.service.i18n.I18nService;
 import fr.paris.lutece.portal.service.message.SiteMessage;
 import fr.paris.lutece.portal.service.message.SiteMessageException;
 import fr.paris.lutece.portal.service.message.SiteMessageService;
 import fr.paris.lutece.portal.service.security.LuteceUser;
 import fr.paris.lutece.portal.service.security.SecurityService;
+import fr.paris.lutece.portal.service.util.AppException;
+import fr.paris.lutece.portal.service.util.AppLogService;
+import fr.paris.lutece.portal.service.workflow.WorkflowService;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
 import fr.paris.lutece.portal.util.mvc.xpage.MVCApplication;
 import fr.paris.lutece.portal.util.mvc.xpage.annotations.Controller;
@@ -43,28 +54,160 @@ public class FormResponseXPage extends MVCApplication
     protected static final String MESSAGE_ERROR_NOT_FOUND_FORM_RESPONSE = "forms.xpage.response.error.notfound";
     protected static final String MESSAGE_FORM_RESPONSE_PAGETITLE = "forms.xpage.response.pagetitle";
     protected static final String MESSAGE_FORM_RESPONSE_PATHLABEL = "forms.xpage.response.pathlabel";
-	
+    private static final String MESSAGE_ACTION_ERROR = "forms.xpage.response.action.error";
+    
     // Views
     private static final String VIEW_FORM_RESPONSE = "formResponseView";
     
+    // Actions
+    private static final String ACTION_PROCESS_ACTION = "doProcessAction";
+    private static final String ACTION_SAVE_TASK_FORM = "doSaveTaskForm";
+    
 	// Templates
     private static final String TEMPLATE_VIEW_FORM_RESPONSE = "/skin/plugins/forms/view_form_response.html";
-
+    private static final String TEMPLATE_TASK_FORM_RESPONSE = "/skin/plugins/forms/task_form_workflow.html";
+    
+    // Marks 
+    private static final String MARK_WORKFLOW_ACTION_LIST = "workflow_action_list";
+    private static final String MARK_ID_FORM_RESPONSE = "id_form_response";
+    private static final String MARK_ID_ACTION = "id_action";
+    private static final String MARK_TASK_FORM = "tasks_form";
+    
+    // Parameters
+    private static final String PARAMETER_ID_ACTION = "id_action";
+    
     @View( value = VIEW_FORM_RESPONSE, defaultView=true )
     public XPage getFormResponseView( HttpServletRequest request ) throws SiteMessageException
     {
     	Locale locale = getLocale( request );
     	FormResponse formResponse = findFormResponseFrom(request);
     	
+    	Collection<Action> actionsList = getActionsForUser( request, formResponse );
+    	
     	Map<String, Object> model = getModel( );
 		model.put( FormsConstants.MARK_FORM_RESPONSE, formResponse );
-    	
+		model.put( MARK_WORKFLOW_ACTION_LIST, actionsList );
+		
     	XPage xPage = getXPage( TEMPLATE_VIEW_FORM_RESPONSE, getLocale( request ), model );
         xPage.setTitle( I18nService.getLocalizedString( MESSAGE_FORM_RESPONSE_PAGETITLE, locale ) );
         xPage.setPathLabel( I18nService.getLocalizedString( MESSAGE_FORM_RESPONSE_PATHLABEL, locale ) );
 
         return xPage;
     }
+    
+    @fr.paris.lutece.portal.util.mvc.commons.annotations.Action( value = ACTION_PROCESS_ACTION )
+    public XPage doProcessAction( HttpServletRequest request )
+    {
+        // Get parameters from request
+        int nIdFormResponse = NumberUtils.toInt( request.getParameter( FormsConstants.PARAMETER_ID_RESPONSE ), NumberUtils.INTEGER_MINUS_ONE );
+        int nIdAction = NumberUtils.toInt( request.getParameter( PARAMETER_ID_ACTION ), NumberUtils.INTEGER_MINUS_ONE );
+
+        LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
+        FormResponse formResponse = FormResponseHome.findByPrimaryKey( nIdFormResponse );
+        
+        if ( user == null || formResponse == null || formResponse.getGuid( ) == null || !formResponse.getGuid( ).equals( user.getName( ) ) )
+        {
+            return redirect( request, VIEW_FORM_RESPONSE, FormsConstants.PARAMETER_ID_RESPONSE, nIdFormResponse );
+        }
+        
+        Locale locale = getLocale( request );
+        WorkflowService workflowService = WorkflowService.getInstance( );
+        if ( workflowService.isDisplayTasksForm( nIdAction, locale ) )
+        {
+            FormsAsynchronousUploadHandler.getHandler( ).removeSessionFiles( request.getSession( ) );
+
+            String strHtmlTasksForm = WorkflowService.getInstance( ).getDisplayTasksForm( nIdFormResponse, FormResponse.RESOURCE_TYPE, nIdAction, request,
+                    locale, null );
+            
+            Map<String, Object> model = new LinkedHashMap<>( );
+            model.put( MARK_ID_FORM_RESPONSE, String.valueOf( nIdFormResponse ) );
+            model.put( MARK_ID_ACTION, String.valueOf( nIdAction ) );
+            model.put( MARK_TASK_FORM, strHtmlTasksForm );
+
+            XPage xPage = getXPage( TEMPLATE_TASK_FORM_RESPONSE, locale, model );
+            xPage.setTitle( I18nService.getLocalizedString( MESSAGE_FORM_RESPONSE_PAGETITLE, locale ) );
+            xPage.setPathLabel( I18nService.getLocalizedString( MESSAGE_FORM_RESPONSE_PATHLABEL, locale ) );
+            
+            return xPage;
+        }
+
+        try
+        {
+            workflowService.doProcessAction( nIdFormResponse, FormResponse.RESOURCE_TYPE, nIdAction, formResponse.getFormId( ), request, locale,
+                    false, user );
+        }
+        catch( AppException e )
+        {
+            AppLogService.error( "Error processing action for id response '" + nIdFormResponse + "' - cause : " + e.getMessage( ), e );
+        }
+        // Redirect to the correct view
+        return redirect( request, VIEW_FORM_RESPONSE, FormsConstants.PARAMETER_ID_RESPONSE, nIdFormResponse );
+    }
+    
+    /**
+     * Process workflow action
+     *
+     * @param request
+     *            The Http request
+     * @return The Jsp URL of the process result
+     */
+    @fr.paris.lutece.portal.util.mvc.commons.annotations.Action( value = ACTION_SAVE_TASK_FORM )
+    public XPage doSaveTaskForm( HttpServletRequest request )
+    {
+        int nIdFormResponse = NumberUtils.toInt( request.getParameter( FormsConstants.PARAMETER_ID_RESPONSE ), NumberUtils.INTEGER_MINUS_ONE );
+        int nIdAction = NumberUtils.toInt( request.getParameter( PARAMETER_ID_ACTION ), NumberUtils.INTEGER_MINUS_ONE );
+
+        LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
+        FormResponse formResponse = FormResponseHome.findByPrimaryKey( nIdFormResponse );
+        
+        if ( user == null || formResponse == null || formResponse.getGuid( ) == null || !formResponse.getGuid( ).equals( user.getName( ) ) )
+        {
+            return redirect( request, VIEW_FORM_RESPONSE, FormsConstants.PARAMETER_ID_RESPONSE, nIdFormResponse );
+        }
+        
+        int nIdForm = formResponse.getFormId( );
+        Locale locale = getLocale( request );
+        WorkflowService workflowService = WorkflowService.getInstance( );
+       
+        if ( workflowService.canProcessAction( nIdFormResponse, FormResponse.RESOURCE_TYPE, nIdAction, nIdForm, request, false, user ) )
+        {
+            try
+            {
+                String strError = workflowService.doSaveTasksForm( nIdFormResponse, FormResponse.RESOURCE_TYPE, nIdAction, nIdForm, request, locale,
+                        user );
+                if ( strError != null )
+                {
+                    return redirect( request, strError );
+                }
+            }
+            catch( AppException e )
+            {
+                AppLogService.error( "Error processing action for record " + nIdFormResponse, e );
+            }
+        }
+        else
+        {
+            addError( MESSAGE_ACTION_ERROR, locale );
+        }
+        return redirect( request, VIEW_FORM_RESPONSE, FormsConstants.PARAMETER_ID_RESPONSE, nIdFormResponse );
+    }
+    	
+	private Collection<Action> getActionsForUser( HttpServletRequest request, FormResponse formResponse )
+	{
+	    LuteceUser user = SecurityService.getInstance( ).getRegisteredUser( request );
+        if ( user != null && formResponse != null && formResponse.getGuid( ) != null && formResponse.getGuid( ).equals( user.getName( ) ) )
+        {
+            Form form = FormHome.findByPrimaryKey( formResponse.getFormId( ) );
+            WorkflowService workflowService = WorkflowService.getInstance( );
+            boolean workflowEnabled = workflowService.isAvailable( ) && ( form.getIdWorkflow( ) != FormsConstants.DEFAULT_ID_VALUE );
+
+            if ( workflowEnabled )
+            {
+                return workflowService.getActions( formResponse.getId( ), FormResponse.RESOURCE_TYPE, form.getIdWorkflow( ), (User) user );
+            }
+        }
+	    return new ArrayList<>( );
+	}
     
     /**
      * Finds the formResponse from the specified request
