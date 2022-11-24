2a15cd50e25f355b2552af2ce945c7356eb92cd4
louis-edmond <louis-edmond.fillet.external@atos.net>
2022-11-24 14:56:43 +0100

    FORMS-847 : modification of order's interface

--- src/java/fr/paris/lutece/plugins/forms/util/FormsConstants.java
+++ src/java/fr/paris/lutece/plugins/forms/util/FormsConstants.java
@@ -162,6 +162,7 @@ public final class FormsConstants
     public static final String PARAMETER_VIEW_MODIFY_CONTROL = "view_modifyControl";
     public static final String PARAMETRE_VALIDATOR_LISTQUESTION_NAME = "forms_listQuestionValidator";
     public static final String PARAMETER_MULTIVIEW_ORDER = "multiview_column_order";
+    public static final String PARAMETER_EXPORT_DISPLAY_ORDER = "export_display_order";
     
     public static final String PARAMETER_USED_CORRECT_RESPONSE = "used_in_correct_form_response";
     public static final String PARAMETER_USED_COMPLETE_RESPONSE = "used_in_complete_form_response";
