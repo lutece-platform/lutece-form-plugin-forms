f41d0f48ad444eea94d44b632bbf67042f968779
Félix Dupriez <felix.dupriez@accenture.com>
2023-03-09 17:21:37 +0100

    #24991 : Improve the validator question service to manage check between question

--- src/java/fr/paris/lutece/plugins/forms/web/entrytype/EntryTypeDefaultDataService.java
+++ src/java/fr/paris/lutece/plugins/forms/web/entrytype/EntryTypeDefaultDataService.java
@@ -118,6 +118,24 @@ public class EntryTypeDefaultDataService implements IEntryDataService
         if ( bValidateQuestion )
         {
             formQuestionResponse.setError( error );
+            
+            List<Control> listControl = ControlHome.getControlByQuestionAndType( question.getId( ), ControlType.VALIDATION.getLabel( ) );
+
+            for ( Control control : listControl )
+            {
+                IValidator validator = EntryServiceManager.getInstance( ).getValidator( control.getValidatorName( ) );
+                if ( !validator.validate( formQuestionResponse, control ) )
+                {
+                    error = new GenericAttributeError( );
+
+                    error.setIsDisplayableError( true );
+                    error.setErrorMessage( control.getErrorMessage( ) );
+
+                    formQuestionResponse.setError( error );
+
+                    break;
+                }
+            }
         }
         else
         {
