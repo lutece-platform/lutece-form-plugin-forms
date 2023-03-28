d745982a578653686d113dc7aee137f1ba5c1f9f
louis-edmond <louis-edmond.fillet.external@atos.net>
2023-03-28 13:10:39 +0200

    LUT-25121 : A form setting that makes it impossible to access the form

--- src/java/fr/paris/lutece/plugins/forms/web/admin/FormJspBean.java
+++ src/java/fr/paris/lutece/plugins/forms/web/admin/FormJspBean.java
@@ -687,12 +687,6 @@ public class FormJspBean extends AbstractJspBean
 
         _formMessage = FormMessageHome.findByForm( _form.getId( ) );
 
-        if ( _form.getLogo( ) != null )
-        {
-            FileHome.remove( _form.getLogo( ).getIdFile( ) );
-            _form.setLogo( null );
-        }
-
         populate( _form, request, request.getLocale( ) );
         populate( _formMessage, request, request.getLocale( ) );
 
@@ -701,6 +695,11 @@ public class FormJspBean extends AbstractJspBean
             return redirect( request, VIEW_MODIFY_FORM, FormsConstants.PARAMETER_ID_FORM, _form.getId( ) );
         }
 
+        if ( _form.getLogo( ) != null )
+        {
+            FileHome.remove( _form.getLogo( ).getIdFile( ) );
+            _form.setLogo( null );
+        }
         _form.setLogo( getLogoFromRequest( request ) );
         if ( _form.getLogo( ) != null )
         {
