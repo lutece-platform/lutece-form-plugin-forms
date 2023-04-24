a5b97a4e5c9ba3b6e8b85f7b0b15a60772120473
Norbert Le Garrec <norbert.legarrec2@paris.fr>
2023-04-24 13:53:08 +0200

    LUT-25749 : Add cartography entry

---
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.3-2.4.4.sql
@@ -0,0 +1,2 @@
+INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+(123,'Cartographie',0,0,0,'forms.entryTypeCartography','map-marked-alt','forms',16,0);
