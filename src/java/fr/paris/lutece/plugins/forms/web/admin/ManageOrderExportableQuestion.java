e3db43067a22f63ce1a71360866c1bf2597cf45a
timothee-hx <timothee.harleaux@atos.net>
2023-06-09 13:44:23 +0200

    redirecting to a jsp when changing order export value

--- src/java/fr/paris/lutece/plugins/forms/web/admin/ManageOrderExportableQuestion.java
+++ src/java/fr/paris/lutece/plugins/forms/web/admin/ManageOrderExportableQuestion.java
@@ -3,10 +3,13 @@ package fr.paris.lutece.plugins.forms.web.admin;
 import fr.paris.lutece.plugins.forms.business.Question;
 import fr.paris.lutece.plugins.forms.business.QuestionHome;
 import fr.paris.lutece.plugins.forms.util.FormsConstants;
+import fr.paris.lutece.portal.service.util.AppLogService;
+import fr.paris.lutece.portal.util.mvc.commons.annotations.Action;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,8 +18,22 @@ public class ManageOrderExportableQuestion {
 
     private static final String PARAMETER_ACTIVE_TAB_PANNEL_2 = "activeTabPannel2";
 
-    public String doMoveUpExportableQuestion(HttpServletRequest request, HttpServletResponse response)
+    @Action( value = "moveUpExportableQuestion")
+    public static HttpServletResponse doMoveUpExportableQuestion(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
+     // print all parameters
+        Map<String, String[]> mapParams = request.getParameterMap( );
+
+        for ( Map.Entry<String, String[]> entry : mapParams.entrySet( ) )
+        {
+            String strParamName = entry.getKey( );
+            String[] tabParamValues = entry.getValue( );
+            for ( String strParamValue : tabParamValues )
+            {
+                AppLogService.info( "Parameter : " + strParamName + " = " + strParamValue );
+            }
+        }
+
         int nIdForm = NumberUtils.toInt( request.getParameter( FormsConstants.PARAMETER_ID_FORM ), FormsConstants.DEFAULT_ID_VALUE );
         int nIdQuestion = NumberUtils.toInt( request.getParameter( FormsConstants.PARAMETER_ID_QUESTION ), FormsConstants.DEFAULT_ID_VALUE );
         int nOrderToSet = NumberUtils.toInt( request.getParameter( FormsConstants.PARAMETER_EXPORT_DISPLAY_ORDER + "_" + nIdQuestion), 0 );
@@ -57,14 +74,15 @@ public class ManageOrderExportableQuestion {
             }
         }
 
-        Map<String, String> mapParameters = new LinkedHashMap<>( );
-        mapParameters.put( FormsConstants.PARAMETER_ID_FORM, String.valueOf( nIdForm ) );
-        mapParameters.put( PARAMETER_ACTIVE_TAB_PANNEL_2, String.valueOf(true));
-        // redirect to the form page
-        // get the base url
-        String strBaseUrl = request.getRequestURL( ).toString( );
+        String strUrl = request.getRequestURL( ).toString();
+        strUrl = strUrl.substring( 0, strUrl.lastIndexOf( "/" ) );
+        strUrl = strUrl+"/"+"ManageFormExport.jsp"+ "?" + "view=manageExport&"+ FormsConstants.PARAMETER_ID_FORM + "=" + 1 + "&" + PARAMETER_ACTIVE_TAB_PANNEL_2 + "=true";
+        System.out.println("URL"+strUrl);
+        // http://localhost:8080/lutece/jsp/admin/plugins/forms/ManageFormExport.jsp?view=manageExport&id_form=1
+        // http://localhost:8080/lutece/jsp/admin/plugins/formsManageFormExport.jsp?view=manageExport&id_form=-1&activeTabPannel2=true
 
-        return strBaseUrl +"/" + FormsConstants.JSP_MANAGE_FORMS + "?" + FormsConstants.PARAMETER_ID_FORM + "=" + nIdForm + "&" + PARAMETER_ACTIVE_TAB_PANNEL_2 + "=true";
+          response.sendRedirect( strUrl);
+       return response;
     }
 
 }
--- webapp/jsp/admin/plugins/forms/ManageOrderExportableQuestion.jsp
+++ webapp/jsp/admin/plugins/forms/ManageOrderExportableQuestion.jsp
@@ -1,10 +1,10 @@
-<%@ page import="fr.paris.lutece.plugins.forms.web.admin.ManageOrderExportableQuestion"%><%@ page errorPage="../../ErrorPagePortal.jsp" trimDirectiveWhitespaces="true" %>
+<%@ page import="fr.paris.lutece.plugins.forms.web.admin.ManageOrderExportableQuestion"%><%@ page errorPage="../../ErrorPagePortal.jsp" trimDirectiveWhitespaces="true" contentType="application/javascript; charset=UTF-8" %>
 
     <jsp:useBean id="manageOrderExportableQuestion" scope="request" class="fr.paris.lutece.plugins.forms.web.admin.ManageOrderExportableQuestion" />
 
 
     <%
         if("doChangeOrderExportableQuestion".equals(request.getParameter("actionName"))) {
-            manageOrderExportableQuestion.doMoveUpExportableQuestion( request, response );
+            ManageOrderExportableQuestion.doMoveUpExportableQuestion(request, response);
         }
     %>
