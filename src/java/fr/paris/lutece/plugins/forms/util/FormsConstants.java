316c97fcb4df52cdbb02352e3fb93147be370f7d
Louis-Edmond <113031723+Louis-Edmond@users.noreply.github.com>
2023-10-06 13:34:14 +0200

    LUT-25079 : Gestion des groupes de travail dans les formulaires (#222)
    Co-authored-by: Seb Leridon <25958061+seboo@users.noreply.github.com>

--- src/java/fr/paris/lutece/plugins/forms/util/FormsConstants.java
+++ src/java/fr/paris/lutece/plugins/forms/util/FormsConstants.java
@@ -102,6 +102,7 @@ public final class FormsConstants
     public static final String MARK_LOGICAL_OPERATOR_LABEL = "logical_operator_label";
     public static final String MARK_REGEX_URL = "regex_url";
     public static final String MARK_UPLOAD_HANDLER = "uploadHandler";
+    public static final String MARK_DEFAULT_VALUE_WORKGROUP_KEY = "workgroup_key_default_value";
 
     // Parameters
     public static final String PARAMETER_PAGE = "page";
