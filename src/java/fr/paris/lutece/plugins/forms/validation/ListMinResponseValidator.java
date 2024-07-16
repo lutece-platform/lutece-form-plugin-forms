c52235b2d35313801d828aee4fb31b8c9cc87024
obfromevi <obfromevi@gmail.com>
2024-07-16 17:44:20 +0200

    LUT-28222 : adding new validation rules for checkbox question

---
+++ src/java/fr/paris/lutece/plugins/forms/validation/ListMinResponseValidator.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2002-2024, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.forms.validation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.paris.lutece.plugins.forms.business.Control;
+import fr.paris.lutece.plugins.forms.business.FormQuestionResponse;
+import fr.paris.lutece.plugins.forms.util.FormsConstants;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+
+/**
+ * 
+ * Validator to check on the minimum number of selected answers
+ *
+ */
+public class ListMinResponseValidator extends AbstractValidator
+{
+
+    private static final String TEMPLATE_DISPLAY_HTML = "/admin/plugins/forms/validators/list_min_responses_template.html";
+	private static final String TEMPLATE_JAVASCRIPT = "/skin/plugins/forms/validators/list_value_javascript.html";
+
+    /**
+     * Constructor of the ListMinResponseValidator
+     * 
+     * @param strValidatorName
+     *            The validator bean name
+     * @param strValidatorDisplayName
+     *            The validator display name
+     * @param listAvailableEntryType
+     *            The list of available entrytype
+     */
+    public ListMinResponseValidator( String strValidatorName, String strValidatorDisplayName, List<String> listAvailableEntryType )
+    {
+        super( strValidatorName, strValidatorDisplayName, listAvailableEntryType );
+    }
+
+    @Override
+    public String getDisplayHtml( Control control )
+    {
+        Map<String, Object> model = new HashMap<>( ); 
+        model.put( FormsConstants.PARAMETER_CONTROL_VALUE, control.getValue( ) );
+        
+        return AppTemplateService.getTemplate( TEMPLATE_DISPLAY_HTML, I18nService.getDefaultLocale( ), model ).getHtml( );
+    }
+
+    @Override
+    public boolean validate( FormQuestionResponse questionResponse, Control control )
+    {
+    	return questionResponse.getEntryResponse( ).size( ) >= Integer.parseInt( control.getValue( ) );       
+    }
+
+    @Override
+    public String getJavascriptValidation( )
+    {
+        return AppTemplateService.getTemplate( TEMPLATE_JAVASCRIPT, I18nService.getDefaultLocale( ), new HashMap<>( ) ).getHtml( );
+    }
+
+    @Override
+    public String getJavascriptControlValue( Control control )
+    {
+        return control.getValue( );
+    }
+}
