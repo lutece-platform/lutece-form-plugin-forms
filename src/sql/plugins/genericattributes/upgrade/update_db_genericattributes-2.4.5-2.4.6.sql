2f6da28ce52bfb4d349d4b1300017d92ae23f9a0
atos-mohamed-b <126660650+atos-mohamed-b@users.noreply.github.com>
2024-08-08 11:32:26 +0200

    LUT-25400: Updated the sortable list to introduce new features allowing the selection/sorting of items between 2 lists

--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.5-2.4.6.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.5-2.4.6.sql
@@ -1,1 +1,8 @@
 ALTER TABLE genatt_entry_type MODIFY COLUMN id_type int AUTO_INCREMENT NOT NULL;
+
+INSERT INTO genatt_field ( id_entry, title, code, VALUE, default_value )
+	SELECT e.id_entry, null, 'sortable_list_type', '0', 0
+	FROM genatt_entry e
+	INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+	WHERE e.resource_type = 'FORMS_FORM'
+	AND t.class_name = 'forms.entryTypeSelectOrder';
