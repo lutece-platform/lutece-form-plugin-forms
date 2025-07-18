b3264663468a13d62a5e326c48fb1035fa2d113e
Jeremy Deloche <jeremy.deloche@atos.com>
2025-07-18 09:28:36 +0200

    LUT-30316 : change the update date for forms response after workflow action

--- src/java/fr/paris/lutece/plugins/forms/service/listener/FormResponseEventListener.java
+++ src/java/fr/paris/lutece/plugins/forms/service/listener/FormResponseEventListener.java
@@ -33,15 +33,21 @@
  */
 package fr.paris.lutece.plugins.forms.service.listener;
 
+import fr.paris.lutece.plugins.forms.business.FormResponse;
+import fr.paris.lutece.plugins.forms.business.FormResponseHome;
 import fr.paris.lutece.plugins.forms.business.form.search.IndexerAction;
 import fr.paris.lutece.plugins.forms.service.FormsPlugin;
 import fr.paris.lutece.plugins.forms.service.event.FormResponseEvent;
 import fr.paris.lutece.plugins.forms.service.search.IFormSearchIndexer;
 import fr.paris.lutece.portal.service.event.EventAction;
 import fr.paris.lutece.portal.service.event.Type;
+import fr.paris.lutece.portal.business.event.ResourceEvent;
+import fr.paris.lutece.portal.service.util.AppLogService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.ObservesAsync;
 import jakarta.inject.Inject;
+import java.sql.Timestamp;
 
 @ApplicationScoped
 public class FormResponseEventListener
@@ -96,5 +102,68 @@ public class FormResponseEventListener
     private void indexFormResponse( FormResponseEvent event, int nIdTask )
     {
     	_formSearchIndexer.indexDocument( event.getFormResponseId( ), nIdTask, FormsPlugin.getPlugin( ) );
+    }
+
+    public void updatedResource( @Observes @Type(EventAction.UPDATE) ResourceEvent event )
+    {
+        updateDateFormResponse( event );
+        indexResource( event, IndexerAction.TASK_MODIFY );
+    }
+
+    /**
+     * Index the forms response
+     *
+     * @param event
+     *            the event
+     * @param nIdTask
+     *            The id task
+     */
+    private void indexResource( ResourceEvent event, int nIdTask )
+    {
+        if ( !checkResourceType( event ) )
+        {
+            return;
+        }
+        try
+        {
+            int nIdResource = Integer.parseInt( event.getIdResource( ) );
+            _formSearchIndexer.indexDocument( nIdResource, nIdTask, FormsPlugin.getPlugin( ) );
+        }
+        catch( NumberFormatException e )
+        {
+            AppLogService.error( "Unable to parse given event id ressource to integer " + event.getIdResource( ), e );
+        }
+    }
+
+    /**
+     * Update the date update of the forms response
+     *
+     * @param event
+     *            the event
+     */
+    private void updateDateFormResponse( ResourceEvent event )
+    {
+        if ( !checkResourceType( event ) )
+        {
+            return;
+        }
+        try
+        {
+            int nIdResource = Integer.parseInt( event.getIdResource( ) );
+
+            FormResponse response = FormResponseHome.findByPrimaryKey( nIdResource );
+            response.setUpdate( new Timestamp( new java.util.Date( ).getTime( ) ) );
+            FormResponseHome.update( response );
+
+        }
+        catch( NumberFormatException e )
+        {
+            AppLogService.error( "Unable to parse given event id ressource to integer " + event.getIdResource( ), e );
+        }
+    }
+
+    private boolean checkResourceType( ResourceEvent event )
+    {
+        return FormResponse.RESOURCE_TYPE.equals( event.getTypeResource( ) );
     }
 }
