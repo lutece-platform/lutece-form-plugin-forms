f6cf5d88cc28d8ae19753a9c8814a6ffc1a1647d
obfromevi <obfromevi@gmail.com>
2025-11-20 15:28:41 +0100

    LUT-29997 : fix WHERE clause and update the init SQL file

--- src/sql/plugins/genericattributes/plugin/init_db_generic_attributes_forms.sql
+++ src/sql/plugins/genericattributes/plugin/init_db_generic_attributes_forms.sql
@@ -5,50 +5,50 @@
 -- Dumping data for table genatt_entry_type
 --
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Bouton radio',0,0,0,'forms.entryTypeRadioButton','dot-circle','forms',5,0);
+('Bouton radio',0,0,0,'forms.entryTypeRadioButton','dot-circle radio','forms',5,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Case à cocher',0,0,0,'forms.entryTypeCheckBox','check-square','forms',6,0);
+('Case à cocher',0,0,0,'forms.entryTypeCheckBox','check-square checkbox','forms',6,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Commentaire',0,1,0,'forms.entryTypeComment','comment','forms',11,0);
+('Commentaire',0,1,0,'forms.entryTypeComment','comment textarea','forms',11,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Date',0,0,0,'forms.entryTypeDate','calendar','forms',4,0);
+('Date',0,0,0,'forms.entryTypeDate','calendar date','forms',4,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Liste déroulante',0,0,0,'forms.entryTypeSelect','list-alt','forms',7,0);
+('Liste déroulante',0,0,0,'forms.entryTypeSelect','list-alt list','forms',7,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Zone de texte court',0,0,0,'forms.entryTypeText','file-alt','forms',2,0);
+('Zone de texte court',0,0,0,'forms.entryTypeText','file-alt text','forms',2,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Zone de texte long',0,0,0,'forms.entryTypeTextArea','sticky-note','forms',3,0);
+('Zone de texte long',0,0,0,'forms.entryTypeTextArea','sticky-note textarea','forms',3,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Fichier',0,0,0,'forms.entryTypeFile','file','forms',9,0);
+('Fichier',0,0,0,'forms.entryTypeFile','file file','forms',9,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Géolocalisation',0,0,0,'forms.entryTypeGeolocation','map-marked-alt','forms',16,0);
+('Géolocalisation',0,0,0,'forms.entryTypeGeolocation','map-marked-alt geoloc','forms',16,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Image',0,0,0,'forms.entryTypeImage','image','forms',10,0);
+('Image',0,0,0,'forms.entryTypeImage','image image','forms',10,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Utilisateur MyLutece',0,0,1,'forms.entryTypeMyLuteceUser','user','forms',13,1);
+('Utilisateur MyLutece',0,0,1,'forms.entryTypeMyLuteceUser','user user','forms',13,1);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Numérotation',0,0,0,'forms.entryTypeNumbering','hashtag','forms',8,0);
+('Numérotation',0,0,0,'forms.entryTypeNumbering','hashtag number','forms',8,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Attribut de l''utilisateur MyLutece',0,0,0,'forms.entryTypeMyLuteceUserattribute','user','forms',14,0);
+('Attribut de l''utilisateur MyLutece',0,0,0,'forms.entryTypeMyLuteceUserattribute','user user','forms',14,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Tableau',0,0,0,'forms.entryTypeArray','table','forms',17,1);
+('Tableau',0,0,0,'forms.entryTypeArray','terminal table','forms',17,1);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Regroupement',1,0,0,'forms.entryTypeGroup','indent','forms',1,0);
+('Regroupement',1,0,0,'forms.entryTypeGroup','info-circle','forms',1,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Conditions d''utilisation', 0, 0, 0, 'forms.entryTypeTermsOfService', 'gavel', 'forms',12,0);
+('Conditions d''utilisation', 0, 0, 0, 'forms.entryTypeTermsOfService', 'futbol checkbox', 'forms',12,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Lecture automatique fichier (OCR)', 0, 0, 0, 'forms.entryTypeAutomaticFileReading', 'file', 'forms',18,1);
+('Lecture automatique fichier (OCR)', 0, 0, 0, 'forms.entryTypeAutomaticFileReading', 'file file', 'forms',18,1);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Camera', 0, 0, 0, 'forms.entryTypeCamera', 'camera', 'forms',15,0);
+('Camera', 0, 0, 0, 'forms.entryTypeCamera', 'camera camera', 'forms',15,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Numéro de téléphone', 0, 0, 0, 'forms.entryTypeTelephoneNumber', 'phone-square', 'forms',19,0);
+('Numéro de téléphone', 0, 0, 0, 'forms.entryTypeTelephoneNumber', 'phone-square phone', 'forms',19,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Nombre', 0, 0, 0, 'forms.entryTypeNumber', 'hashtag', 'forms',5,0);
+('Nombre', 0, 0, 0, 'forms.entryTypeNumber', 'hashtag number', 'forms',5,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Liste triable', 0, 0, 0, 'forms.entryTypeSelectOrder', 'list-ol', 'forms',20,0);
+('Liste triable', 0, 0, 0, 'forms.entryTypeSelectOrder', 'list-ol list', 'forms',20,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Galerie image', 0, 0, 0, 'forms.entryTypeGalleryImage', 'image', 'forms',21,1);
+('Galerie image', 0, 0, 0, 'forms.entryTypeGalleryImage', 'image image', 'forms',21,1);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Creneau horaire', 0, 0, 0, 'forms.entryTypeSlot', 'calendar', 'forms',22,0);
+('Creneau horaire', 0, 0, 0, 'forms.entryTypeSlot', 'calendar time', 'forms',22,0);
 INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-('Session',0,0,0,'forms.entryTypeSession','user','forms',23,0);
+('Session',0,0,0,'forms.entryTypeSession','map-marked-alt id-badge-2','forms',23,0);
--- src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.6-4.0.0.sql
+++ src/sql/plugins/genericattributes/upgrade/update_db_genericattributes-2.4.6-4.0.0.sql
@@ -1,28 +1,28 @@
---liquibase formatted sql
---changeset forms:update_db_genericattributes-2.4.6-3.1.2.sql
---preconditions onFail:MARK_RAN onError:WARN
-UPDATE genatt_entry_type SET icon_name='dot-circle radio' WHERE id_type=101;
-UPDATE genatt_entry_type SET icon_name='check-square checkbox' WHERE id_type=102;
-UPDATE genatt_entry_type SET icon_name='comment textarea' WHERE id_type=103;
-UPDATE genatt_entry_type SET icon_name='calendar date' WHERE id_type=104;
-UPDATE genatt_entry_type SET icon_name='list-alt list' WHERE id_type=105;
-UPDATE genatt_entry_type SET icon_name='file-alt text' WHERE id_type=106;
-UPDATE genatt_entry_type SET icon_name='sticky-note textarea' WHERE id_type=107;
-UPDATE genatt_entry_type SET icon_name='file file' WHERE id_type=108;
-UPDATE genatt_entry_type SET icon_name='map-marked-alt geoloc' WHERE id_type=109;
-UPDATE genatt_entry_type SET icon_name='image image' WHERE id_type=110;
-UPDATE genatt_entry_type SET icon_name='user user' WHERE id_type=111;
-UPDATE genatt_entry_type SET icon_name='hashtag number' WHERE id_type=112;
-UPDATE genatt_entry_type SET icon_name='user user' WHERE id_type=113;
-UPDATE genatt_entry_type SET icon_name='terminal table' WHERE id_type=114;
-UPDATE genatt_entry_type SET icon_name='info-circle' WHERE id_type=115;
-UPDATE genatt_entry_type SET icon_name='futbol checkbox' WHERE id_type=116;
-UPDATE genatt_entry_type SET icon_name='file file' WHERE id_type=117;
-UPDATE genatt_entry_type SET icon_name='camera camera' WHERE id_type=118;
-UPDATE genatt_entry_type SET icon_name='phone-square phone' WHERE id_type=119;
-UPDATE genatt_entry_type SET icon_name='hashtag number' WHERE id_type=120;
-UPDATE genatt_entry_type SET icon_name='list-ol list' WHERE id_type=121;
-UPDATE genatt_entry_type SET icon_name='image image' WHERE id_type=122;
-UPDATE genatt_entry_type SET icon_name='map-marked-alt carto' WHERE id_type=123;
-UPDATE genatt_entry_type SET icon_name='calendar time' WHERE id_type=124;
-UPDATE genatt_entry_type SET icon_name='map-marked-alt id-badge-2' WHERE id_type=125;
+-- liquibase formatted sql
+-- changeset forms:update_db_genericattributes-2.4.6-4.0.0.sql
+-- preconditions onFail:MARK_RAN onError:WARN
+UPDATE genatt_entry_type SET icon_name='dot-circle radio' WHERE class_name='forms.entryTypeRadioButton';
+UPDATE genatt_entry_type SET icon_name='check-square checkbox' WHERE class_name='forms.entryTypeCheckBox';
+UPDATE genatt_entry_type SET icon_name='comment textarea' WHERE class_name='forms.entryTypeComment';
+UPDATE genatt_entry_type SET icon_name='calendar date' WHERE class_name='forms.entryTypeDate';
+UPDATE genatt_entry_type SET icon_name='list-alt list' WHERE class_name='forms.entryTypeSelect';
+UPDATE genatt_entry_type SET icon_name='file-alt text' WHERE class_name='forms.entryTypeText';
+UPDATE genatt_entry_type SET icon_name='sticky-note textarea' WHERE class_name='forms.entryTypeTextArea';
+UPDATE genatt_entry_type SET icon_name='file file' WHERE class_name='forms.entryTypeFile';
+UPDATE genatt_entry_type SET icon_name='map-marked-alt geoloc' WHERE class_name='forms.entryTypeGeolocation';
+UPDATE genatt_entry_type SET icon_name='image image' WHERE class_name='forms.entryTypeImage';
+UPDATE genatt_entry_type SET icon_name='user user' WHERE class_name='forms.entryTypeMyLuteceUser';
+UPDATE genatt_entry_type SET icon_name='hashtag number' WHERE class_name='forms.entryTypeNumbering';
+UPDATE genatt_entry_type SET icon_name='user user' WHERE class_name='forms.entryTypeMyLuteceUserattribute';
+UPDATE genatt_entry_type SET icon_name='terminal table' WHERE class_name='forms.entryTypeArray';
+UPDATE genatt_entry_type SET icon_name='info-circle' WHERE class_name='forms.entryTypeGroup';
+UPDATE genatt_entry_type SET icon_name='futbol checkbox' WHERE class_name='forms.entryTypeTermsOfService';
+UPDATE genatt_entry_type SET icon_name='file file' WHERE class_name='forms.entryTypeAutomaticFileReading';
+UPDATE genatt_entry_type SET icon_name='camera camera' WHERE class_name='forms.entryTypeCamera';
+UPDATE genatt_entry_type SET icon_name='phone-square phone' WHERE class_name='forms.entryTypeTelephoneNumber';
+UPDATE genatt_entry_type SET icon_name='hashtag number' WHERE class_name='forms.entryTypeNumber';
+UPDATE genatt_entry_type SET icon_name='list-ol list' WHERE class_name='forms.entryTypeSelectOrder';
+UPDATE genatt_entry_type SET icon_name='image image' WHERE class_name='forms.entryTypeGalleryImage';
+UPDATE genatt_entry_type SET icon_name='map-marked-alt carto' WHERE class_name='forms.entryTypeCartography';
+UPDATE genatt_entry_type SET icon_name='calendar time' WHERE class_name='forms.entryTypeSlot';
+UPDATE genatt_entry_type SET icon_name='map-marked-alt id-badge-2' WHERE class_name='forms.entryTypeSession';
