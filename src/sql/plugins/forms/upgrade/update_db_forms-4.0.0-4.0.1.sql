f95bb9feca9e21d0c9a6fbb0ad532e031a5d67f9
evdnjde <jeremy.deloche@atos.net>
2026-02-16 16:27:36 +0100

    LUT-31751 : Refactor Group display management

--- src/sql/plugins/forms/upgrade/update_db_forms-4.0.0-4.0.1.sql
+++ src/sql/plugins/forms/upgrade/update_db_forms-4.0.0-4.0.1.sql
@@ -10,3 +10,9 @@ ALTER TABLE forms_form ADD backup_storage_period INT DEFAULT -1 NOT NULL;
 -- precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=database() AND TABLE_NAME='forms_step' AND COLUMN_NAME='is_title_displayed';
 ALTER TABLE forms_step ADD is_title_displayed SMALLINT default 1 NOT NULL;
 UPDATE forms_step SET is_title_displayed=0, title=REPLACE(title, ' - hidden', '') where title like '% - hidden';
+
+-- changeset forms:update_db_forms-4.0.0-4.0.1-rev2.sql
+-- preconditions onFail:MARK_RAN onError:WARN
+-- precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA=database() AND TABLE_NAME='forms_group' AND COLUMN_NAME='is_title_displayed';
+ALTER TABLE forms_group ADD is_title_displayed SMALLINT default 1 NOT NULL;
+UPDATE forms_group SET is_title_displayed=0, title=REPLACE(title, ' - hidden', '') where title like '% - hidden';
