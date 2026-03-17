425a1426e3432a96fca1de7a19f549a75cf4e19f
obfromevi <obfromevi@gmail.com>
2026-03-17 16:38:52 +0100

    LUT-32160 : fix redirect URL and HTTP 500 when auth is required and end message is disabled

--- src/java/fr/paris/lutece/plugins/forms/web/FormXPage.java
+++ src/java/fr/paris/lutece/plugins/forms/web/FormXPage.java
@@ -916,7 +916,7 @@ public class FormXPage extends MVCApplication
 
             if ( form.isAuthentificationNeeded( ) )
             {
-                url= bIsEndMessageDisplayed? new UrlItem( AppPathService.getPortalUrl( ) ):new UrlItem( "" );
+                url= bIsEndMessageDisplayed? new UrlItem( AppPathService.getPortalUrl( ) ):new UrlItem( "Portal.jsp" );
                 url.addParameter( MVCUtils.PARAMETER_PAGE, FormResponseXPage.XPAGE_NAME );
                 url.addParameter( MVCUtils.PARAMETER_VIEW, FormResponseXPage.VIEW_FORM_RESPONSE );
                 url.addParameter( FormsConstants.PARAMETER_ID_RESPONSE, nIdFormResponse );
