e7e8ef9a39d65f4d26eeb5588963edbfa7c4d7a1
evdnjde <jeremy.deloche@atos.net>
2026-03-20 16:27:40 +0100

    LUT-31712 : Forms parameters ergonomy

--- src/java/fr/paris/lutece/plugins/forms/web/admin/FormMultiviewConfigJspBean.java
+++ src/java/fr/paris/lutece/plugins/forms/web/admin/FormMultiviewConfigJspBean.java
@@ -93,6 +93,9 @@ public class FormMultiviewConfigJspBean extends AbstractJspBean
     private static final String VIEW_MANAGE_MULTIVIEW = "manageMultiview";
     private static final String VIEW_MANAGE_FORMS = "ManageForms.jsp?view=manageExport";
 
+    // Infos
+    private static final String INFO_MULTIVIEW_UPDATED = "forms.info.multiview.updated";
+
     // Marks
     private static final String MARK_FORM = "form";
     private static final String MARK_FILTERABLE_QUESTIONLIST = "filterableQuestionList";
@@ -188,6 +191,7 @@ public class FormMultiviewConfigJspBean extends AbstractJspBean
         Map<String, String> mapParameters = new LinkedHashMap<>( );
         mapParameters.put( FormsConstants.PARAMETER_ID_FORM, String.valueOf( nId ) );
 
+        addInfo( INFO_MULTIVIEW_UPDATED, getLocale( ) );
         return redirect( request, VIEW_MANAGE_MULTIVIEW, mapParameters );
     }
     
@@ -223,6 +227,7 @@ public class FormMultiviewConfigJspBean extends AbstractJspBean
         Map<String, String> mapParameters = new LinkedHashMap<>( );
         mapParameters.put( FormsConstants.PARAMETER_ID_FORM, String.valueOf( nId ) );
 
+        addInfo( INFO_MULTIVIEW_UPDATED, getLocale( ) );
         return redirect( request, VIEW_MANAGE_MULTIVIEW, mapParameters );
     }
 
