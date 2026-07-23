5546e1e1af3fe213f92681277cfff19daa74e9c1
Antoine Fuzier <afuzier@yupiik.com>
2026-07-23 17:12:56 +0200

    LUT-32262 : Delete useless parameter

--- src/java/fr/paris/lutece/plugins/forms/web/FormXPage.java
+++ src/java/fr/paris/lutece/plugins/forms/web/FormXPage.java
@@ -816,7 +816,7 @@ public class FormXPage extends MVCApplication
 
         FormMessage formMessage = FormMessageHome.findByForm( form.getId( ) );
         boolean bIsEndMessageDisplayed = formMessage.getEndMessageDisplay( );
-        String strBackUrl = getBackUrl( form, bIsEndMessageDisplayed, _formResponseManager.getFormResponse( ).getId( ) );
+        String strBackUrl = getBackUrl( form, bIsEndMessageDisplayed );
         initAfterSave( request );
 
         if ( formMessage.getEndMessageDisplay( ) )
@@ -904,7 +904,7 @@ public class FormXPage extends MVCApplication
      *            {@code true} if the end message is displayed, {@code false} otherwise
      * @return the back URL
      */
-    private String getBackUrl( Form form, boolean bIsEndMessageDisplayed, int nIdFormResponse )
+    private String getBackUrl( Form form, boolean bIsEndMessageDisplayed )
     {
         if ( StringUtils.isNotEmpty( form.getReturnUrl( ) ) )
         {
