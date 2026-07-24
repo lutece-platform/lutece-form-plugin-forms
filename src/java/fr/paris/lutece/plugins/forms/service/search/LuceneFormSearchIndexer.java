d3349366a495e111097bc73bab8321ebff5902ad
Antoine Fuzier <afuzier@yupiik.com>
2026-07-24 15:35:39 +0200

    LUT-32841 : Add control in form indexer

--- src/java/fr/paris/lutece/plugins/forms/service/search/LuceneFormSearchIndexer.java
+++ src/java/fr/paris/lutece/plugins/forms/service/search/LuceneFormSearchIndexer.java
@@ -574,6 +574,13 @@ public class LuceneFormSearchIndexer implements IFormSearchIndexer
         {
             Document doc = null;
             Form form = mapForms.get( formResponse.getFormId( ) );
+            if ( form == null )
+            {
+                AppLogService.error( "Skipping indexation of form response {}: associated form {} not found", formResponse.getId( ),
+                        formResponse.getFormId( ) );
+                continue;
+            }
+
             State formResponseState = null;
             if ( !_stateServiceInstance.isUnsatisfied( ) )
             {
