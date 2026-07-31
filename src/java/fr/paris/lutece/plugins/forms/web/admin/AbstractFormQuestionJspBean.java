87e10c45485ad1143841dd99257a897b0161eedb
Antoine Fuzier <afuzier@yupiik.com>
2026-07-31 14:37:20 +0200

    LUT-33049 : Fix order of responses in CSV/ export

--- src/java/fr/paris/lutece/plugins/forms/web/admin/AbstractFormQuestionJspBean.java
+++ src/java/fr/paris/lutece/plugins/forms/web/admin/AbstractFormQuestionJspBean.java
@@ -271,7 +271,7 @@ public abstract class AbstractFormQuestionJspBean extends AbstractJspBean
      *            The URL to go to after displaying an error message
      * @return The URL to go after performing the action
      */
-    protected String processQuestionUpdate( HttpServletRequest request, String errorRetrunUrl ) throws CodeAlreadyExistsException
+    protected String processQuestionUpdate( HttpServletRequest request, String errorReturnUrl ) throws CodeAlreadyExistsException
     {
         String strIdStep = request.getParameter( FormsConstants.PARAMETER_ID_STEP );
         int nIdStep = NumberUtils.toInt( strIdStep, INTEGER_MINUS_ONE );
@@ -291,7 +291,7 @@ public abstract class AbstractFormQuestionJspBean extends AbstractJspBean
         int nIdEntry = _question.getIdEntry( );
         _entry = getFormDatabaseService( ).findEntryByPrimaryKey( nIdEntry );
 
-        String strError = EntryTypeServiceManager.getEntryTypeService( _entry ).getRequestData( _entry, request, getLocale( ), errorRetrunUrl );
+        String strError = EntryTypeServiceManager.getEntryTypeService( _entry ).getRequestData( _entry, request, getLocale( ), errorReturnUrl );
 
         if ( strError != null )
         {
